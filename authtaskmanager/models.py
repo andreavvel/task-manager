c159f21d1abd0f4af0eafe5201997d52fba54ef6
Andrea Velásquez <andreagvj@gmail.com>
2024-02-14 14:55:40 +0100

    changing the database

--- authtaskmanager/models.py
+++ authtaskmanager/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_regularuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    #rn we use the is regular user bool to check which user is which
+
+class Auser(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    tasks = models.ManyToManyField(Task, through='TakenQuiz') 
